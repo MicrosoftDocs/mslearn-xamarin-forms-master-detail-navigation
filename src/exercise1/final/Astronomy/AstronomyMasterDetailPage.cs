5a333b5ca0a53c1a76f6b6256bde1dc8ef0112e3
Adam Patridge <patridge@gmail.com>
2019-07-10 10:42:30 -0600

    [ex1-final] Curly braces to match text content

--- src/exercise1/final/Astronomy/AstronomyMasterDetailPage.cs
+++ src/exercise1/final/Astronomy/AstronomyMasterDetailPage.cs
@@ -10,7 +10,9 @@ namespace Astronomy
             var master = new AstronomyMasterPage();
 
             if (Device.RuntimePlatform == Device.iOS)
+            {
                 master.IconImageSource = ImageSource.FromFile("nav-menu-icon.png");
+            }
 
             this.Master = master;
 
